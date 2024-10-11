6baa143b5ea79f2564a6d54267102727ae0fcc67
Femi Ashiru <femi.ashiru@bjss.com>
2024-10-11 15:36:57 +0100

    write test to render QuoteLoader

--- src/QuoteLoader.spec.jsx
+++ src/QuoteLoader.spec.jsx
@@ -1,6 +1,9 @@
 import { render, screen } from "@testing-library/react";
 import { describe, expect } from "vitest";
 
-describe("Quote Loader", ()=>{
+describe("Quote Loader", () => {
 
-})
+    it("renders", ()=> {
+        render(<QuoteLoader />)
+    });
+});
