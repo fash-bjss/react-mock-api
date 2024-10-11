bc888cbb1587afb573fa9ff233b5be1dd4b785c8
Femi Ashiru <femi.ashiru@bjss.com>
2024-10-11 15:53:00 +0100

    add fake api loader

--- src/QuoteLoader.spec.jsx
+++ src/QuoteLoader.spec.jsx
@@ -1,8 +1,32 @@
+import * as React from "react"
+
 import { render, screen } from "@testing-library/react";
 import { describe, expect } from "vitest";
 import QuoteLoader from "./QuoteLoader";
 
+//MockServiceWorker
+import { HttpResponse, http , httpResponse } from "msw"
+import { setupServer } from "msw/node"
+
+
+// https://quotes.rest/random
+
+//This will contain both real and mock api routes
+const apiRoutes = [
+    http.get("https://mock-api.com/quoteoftheday", ()=> {
+        return HttpResponse.json({
+            text: "Hello everybody"
+        });
+    }),
+
+]
+
+const stubQuoteApi = setupServer(...apiRoutes)
+
 describe("Quote Loader", () => {
+
+    // Start fake loader at top level of test
+    stubQuoteApi.listen();
 
     it("renders", ()=> {
         render(<QuoteLoader />)
