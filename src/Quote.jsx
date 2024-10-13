72a039dcf09300dc5177d52d329675c8c8c91f00
Femi Ashiru <femi.ashiru@bjss.com>
2024-10-13 20:21:21 +0100

    update quote with handler to change btn text and pass test

--- src/Quote.jsx
+++ src/Quote.jsx
@@ -1,10 +1,21 @@
+import { useState } from "react"
+
 const Quote = ({text}) => {
+
+    const [liked, setLiked] = useState(false)
+
+    const handleButton = () => {
+        
+        setLiked(!liked)
+    }
+
+    const btnText = liked ? "Liked" : "Like"
 
     return(
         <>
             <h2>Quote of the Day</h2>
             <p>{text}</p>
-            <button>"Like"</button>
+            <button onClick={handleButton}>{btnText}</button>
         </>
     )
 }
